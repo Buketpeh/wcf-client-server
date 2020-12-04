3dccbcfe9d065e1467bf82d339fd201e822eb675
Buket PEHLİVAN <buketpeh@gmail.com>
2020-12-04 09:46:03 +0300

    .

---
+++ wcf-sample-server-client-master/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To add ServerServiceClient use
+            //svcutil.exe http://localhost:8000/ServerService?wsdl
+            //OR easier way 
+            //To add new right click on project "Add Service Reference"
+            //Update right click on "Service References->ServiceReference1" then "Update Service Reference"
+            ServerServiceClient client = new ServerServiceClient("BasicHttpBinding_IServerService", "http://localhost:8000/ServerService");
+           
+            client.Test("Connected.");
+
+            Console.WriteLine("What's your name:");
+            var sessionName = Console.ReadLine();
+
+            client.StoreSession(sessionName);
+            Console.WriteLine(client.GetSessionName());
+            Console.ReadLine();
+
+            client.SendString(sessionName);
+            Console.WriteLine("Uploaded");
+
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
---
+++ wcf-sample-server-client-master/Server/ServerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server
+{
+    public class ServerService : IServerService
+    {
+        private string _sessionName = "";
+
+        public void StoreSession(string sessionName)
+        {
+            _sessionName = sessionName;
+        }
+
+        public string GetSessionName()
+        {
+            return _sessionName;
+        }
+
+        public void SendString(string name)
+        {
+            Console.WriteLine($"Received: {name}");
+        }
+
+        public bool Test(string input)
+        {
+            Console.WriteLine($"Test: {input}");
+            return true;
+        }
+    }
+}
